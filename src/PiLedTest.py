a85c5c9f006b0f57eb2706a70d24b4b5c3e69237
Chuu Myet Thwe <CHUUMYET.22@ichat.sp.edu.sg>
2023-11-12 09:57:48 +0800

    Updated for Lab4

---
+++ src/PiLedTest.py
@@ -0,0 +1,30 @@
+import socket
+import time
+from time import sleep
+
+from hal import hal_input_switch as switch
+from hal import hal_led as leds
+from time import sleep
+
+def main():
+    switch.init()
+    leds.init()
+    i = 0
+
+    while True:
+        if switch.read_slide_switch() == 1:
+            leds.set_output(24,1)
+            sleep(0.1)
+            leds.set_output(24,0)
+            sleep(0.1)
+            i = 0
+        else:
+            if i < 25:
+                i += 1
+                sleep(0.05)
+                leds.set_output(24,1)
+                sleep(0.05)
+                leds.set_output(24,0)
+
+if __name__ == "__main__":
+    main()
